2b205fd061e37134c6ac60fcd64f66db82abb1fc
ishaaq08 <ishaaq001@gmail.com>
2023-09-13 14:12:19 +0100

    index.js changes

--- index.js
+++ index.js
@@ -1,76 +1,40 @@
 
-// // 1) Install nodemon -  Avoid constant manual restart of server 
+/*
+Install nodemon -  Avoid constant manual restart of server 
 
-// // 2) change scripts in package.json file: dev and start
+Change scripts in package.json: scripts > dev and start
 
-// // npm run dev in terminal -  anytime a change is made to the doc it runs again
+In the terminal to run the server input 'npm run dev'
 
-// // import 
-// // create server that can respond to request
-// // create server listening
+Server(request, response) :
+    Request > from the client to the server
+    Response > from the server to the client i.e. logging on the screen
+*/
 
-// // Importing http modules from node
-
-// const { log } = require("console")
-// const http = require("http")
-
-// // Create a server and sends the response: (req=object, res=object)
-
-// const server = http.createServer((req, res)=>{
-    
-//     console.log(req.url)
-//     console.log(req.method)
-
-//     // Set the header - tell the browser that the following content will be HTML
-//     res.setHeader("Content-Type", "text/html")
-
-
-//     // What to do when no more data is coming
-//     res.statusCode = 404
-//     res.end("<img src='https://pbs.twimg.com/media/Dj8XlmjV4AEzsvr.jpg'>")
-// }
-// )
-
-// // Start out server - tell the server to listen 
-
-// server.listen(3000, () => {
-//     console.log("Server Ready")
-// }
-// )
-
-// Import express js
 
 require("dotenv").config()
 
+// Installed and imported the Express JS package via Node Package(module) Manager
+
 const express = require("express")
 
-// Create our server
+// Create our server through the express() function which is imported via Express JS 
+
 const app = express()
 
 //  Setup a port
 const port = process.env.PORT
 
-// Imports fruit json file 
+// Imported the fruit.json file - this file was an array of objects 
+
 const fruits = require("./fruits.json")
-
-// middleware 
-
-// app.use((req, res, next) => {
-//     console.log("I am a piece of Midleware")
-//     next()
-// })
-
-// app.use((req, res, next) => {
-//     console.log("I am also a piece of Midleware")
-//     next()
-// })
 
 // Express.JSON 
     // Every single request will hit that middle ware
 
 app.use(express.json())
 
-// Define the roots 
+// Simple GET requests
 
 app.get("/", (req,res) => {
     res.send("Hello Fruit API")
@@ -80,107 +44,112 @@ app.get("/fruits", (req,res) => {
     res.send(fruits)
 })
 
-// body - contains the information we will use
-
-const getFruitIndex = name => {
-    // Take in a lowercase fruit name and return the index of the fruit or -1
-
-    // By default findIndex will return -1 if it cant find anything 
-
-    return fruits.findIndex((fruit) => fruit.name.toLowerCase() == name)
-}
-
-app.post("/fruits", (req, res) => {
-
-    const fi = getFruitIndex(req.body.name.toLowerCase())
-    if(fi > -1){
-        res.status(409).send("The fruit already exists")
-    } else {
-        // fruits.push(req.body)
-        // res.status(201).send(req.body)
-
-        // Create an array of all ids
-        const ids = fruits.map((fruit) => fruit.id)
-
-        // Get the maximum id
-        let maxId = Math.max(...ids)
-
-        // Increment that by one 
-        maxId ++
-
-        // Adjust id to new max id
-        req.body.id = maxId
-
-        fruits.splice(fi,1)
-        res.sendStatus(200)
-    }
-
-    const fruit = req.body
-    console.log(fruit)
-    res.send("New Fruit Created")
-})
-
-app.delete("/fruits/:name", (req, res) => {
-    const fi =getFruitIndex(req.params.name.toLowerCase())
-    if(fi == -1){
-        res.status(404).send("Fruit cannot be found")
-    }else{
-        
-    }
-})
-
-// req.params > Returns an object hence why we use object dot notation to access the value of the 'name' key
+/*
+Creating a GET request which returns the result for a specific fruit. 
+*/
 
 app.get("/fruits/:name", (req,res) => {
-    // res.send(`Return a fruit with ${req.params.name} name`)
-    
-    // Define a variable that records the fruit name the user is searching for
     const name = req.params.name.toLowerCase()
     
-    // Now search for the fruit - match param name with a name in fruits.json
+    // Search through the fruits.json file for a fruit that matches the users input 
+
     const fruit = fruits.find(fruit => fruit.name.toLowerCase() == name)
-  
-    // Displaying the fruit if it does exist 
+   
     if(fruit == undefined){
         // If there is no match
-        res.status(404).send("The fruit doesn't exist")
+        res.status(404).send(`${req.params.name}: does not exist as a fruit`)
     }else{
         res.send(fruit)
     }
 })
 
-// We now want the server to return a particular fruit 
+/*
+WHAT - This function will search for the index of a fruit inputted by the user 
+RESULT - the index of the inputted fruit: TRUE = >-1, FALSE = -1
+*/
+
+const getFruitIndex = name => {
+    // Take in a lowercase fruit name and return the index of the fruit or -1
+    // 'fruit' is the iterative variable taking on the value of each object with each iteration
+
+    /*
+    Iterating through the array 
+    With each iteration 'fruit' is equal to the next object 
+    The function then checks if the name property of the iterative variable is the same as the argument 
+    The argument is 'name' and thats what the user either wants to delete or post
+    */
+    return fruits.findIndex(fruit => fruit.name.toLowerCase() == name)
+}
+
+app.post("/fruits", (req, res) => {
+
+    /*
+    Creating a variable 
+    Invoking the getFruitIndex with an argument of req.body.name.toLowerCase()
+    The argument will be the fruit that the person wants to add to the json file - unsure about method of submission
+    It will return a number
+    */
+    const fi = getFruitIndex(req.body.name.toLowerCase())
+
+    if(fi > -1){
+        res.status(409).send("The fruit already exists")
+    } else {
+
+        /*
+        We need to insert the inputted value in to the array.
+        We can't just push it to the end however as every object in the array has a unique ID. 
+        Therefore, we calculate the maximum ID, increment it by 1 and assign it to the new value.   
+        */
+
+        const ids = fruits.map((fruit) => fruit.id)
+        let maxId = Math.max(...ids)
+        maxId ++
+        req.body.id = maxId
+
+        const newFruit = req.body
+        fruits.push(newFruit)
+
+        //res.status(201).send(fruit)
+        console.log(fruit)
+        res.send(`${req.body} was created as a new Fruit `)
+    }
+})
 
 
-// Make the server listen
+/* 
+Defining a route so the user can delete a fruit
+*/
+
+app.delete("/fruits/:name", (req, res) => {
+
+    /*
+    > WHAT - Checking if the parameter exists in the json file.
+    > WHY - You can only delete the fruit if it exists! If it doesn't exist the function will return -1. 
+
+    REMEMBER -
+         getFruitIndex(arg=name)
+         req.params {
+            name: "..."
+         }
+    */
+
+    const fi = getFruitIndex(req.params.name.toLowerCase())
+
+    if(fi == -1){
+        res.status(404).send("Fruit cannot be found")
+    }else{
+        fruits.splice(fi,1)
+        // res.sendStatus(200)
+        res.send("Item deleted sucessfully!")
+    }
+})
+
+
+// Turn on the server so that it listens out for requests 
 
 app.listen(port, () => {
     console.log(`Server is now listening on port ${port}`);
 })
 
 
-// app.get("/params", (req,res) => {
-//     res.send(req.params)
-// })
-
-
-//     // forward slash represents the root e.g. matilda.com
-// app.get(`/`, (req,res) => {
-//     res.send("Hello world")
-// })
-
-// app.get("/chicken", (req,res) => {
-//     res.send("Hello Chicken")
-// })
-
-// // Parameters - route out specific things
-//     // Queries come after a question mark in a url
-
-// app.get("/chicken/:name", (req,res) => {
-//     res.send(req.params)
-// })
-
-// app.get("/example", (req, res) => {
-//     res.sendStatus(418)
-// })
 
